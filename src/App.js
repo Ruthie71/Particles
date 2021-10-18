a8e34eef19f19981d43eb141e2e6ed1a3b52b444
Ruthie <ruthmckinney1971@gmail.com>
2021-10-18 15:55:58 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Particles from "react-particles-js";
+import undraw_welcome_cats_thqn from "./SVG/undraw_welcome_cats_thqn.svg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div>
+            <div className="scale-in-center" id="welcome">
+                <img src={undraw_welcome_cats_thqn} alt="welcome cats" />
+            </div>
+            <Particles
+                id="particles-js"
+                params={{
+                    particles: {
+                        number: {
+                            value: 400,
+                            density: {
+                                enable: true,
+                                value_area: 1000,
+                            },
+                        },
+                        color: {
+                            value: "#fff",
+                        },
+                        opacity: {
+                            value: 0.5,
+                            anim: {
+                                enable: true,
+                            },
+                        },
+                        size: {
+                            value: 7,
+                            random: true,
+                            anim: {
+                                enable: true,
+                                speed: 3,
+                            },
+                        },
+                        line_linked: {
+                            enable: false,
+                        },
+                        move: {
+                            speed: 0.2,
+                        },
+                    },
+                }}
+            />
+        </div>
+    );
+};
 
 export default App;
